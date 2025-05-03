56ed52eb2cbf0ff49bf7b46a5794a00c466906c1
goodnesslight <evgenijmajboroda1@gmail.com>
2025-05-03 13:01:09 +0300

    Update: Reworked

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,21 +1,19 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app/app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
+
+  console.log('asdasd');
+  app.useWebSocketAdapter(new IoAdapter(app));
+  await app.listen(3000);
 }
 
 bootstrap();
